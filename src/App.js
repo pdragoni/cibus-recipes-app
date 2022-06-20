69eadbaf85e4ebcaea5094dd98a64cda77ecc0c4
Pedro Caldas <pedronerislc@gmail.com>
2022-06-20 17:59:23 -0300

    Finaliza página de Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Provider from './context/Provider';
 import Login from './pages/Login';
+import Foods from './pages/Foods';
 
 function App() {
   return (
     <Provider>
-      <h1>Teste</h1>
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/foods" component={ Foods } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
