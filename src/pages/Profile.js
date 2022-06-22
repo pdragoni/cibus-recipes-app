6e72bb374b9d15138db9b8924ae671311f88c33a
Pedro Caldas <pedronerislc@gmail.com>
2022-06-22 15:10:31 -0300

    Adiciona à página de Perfil os botões de receitas e de logout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Context from '../context/Context';
@@ -6,30 +7,58 @@ import Context from '../context/Context';
 function Profile() {
   const title = 'Profile';
   const { setPageTitle, setSearchPageButton } = useContext(Context);
+  const [usuario, setUsuario] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     setPageTitle(title);
     setSearchPageButton(false);
   }, []);
 
+  useEffect(() => {
+    const email = JSON.parse(localStorage.getItem('user'));
+    if (email) {
+      setUsuario(email.email);
+    }
+  }, []);
+
+  const logoutFunction = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('cocktailsToken');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('inProgressRecipes');
+    history.push('/');
+  };
+
   return (
     <div>
       <Header />
       <body>
-        <p>Name</p>
-        <p>Email</p>
-        <p>Link pra alterar senha</p>
+        <div data-testid="profile-email">
+          {usuario}
+        </div>
         <button
           type="button"
-          onClick={ () => { console.log('Comidas'); } }
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
         >
-          Comidas
+          Done Recipes
         </button>
         <button
           type="button"
-          onClick={ () => { console.log('bebidas'); } }
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
         >
-          Bebidas
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ logoutFunction }
+        >
+          Logout
         </button>
       </body>
       <Footer />
